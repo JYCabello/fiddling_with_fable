c3b35a891d988adcba4503270664968aee5e859f
JYCabello <j.cabello.ramos@gmail.com>
2020-12-28 16:25:50 +0100

    Some fs fiddling

--- FableApp/src/App.fs
+++ FableApp/src/App.fs
@@ -8,7 +8,18 @@ let mutable count = 0
 // Get a reference to our button and cast the Element to an HTMLButtonElement
 let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
 
+let countAttrName = "data-count"
+
+myButton.setAttribute(countAttrName, string 0)
+
+let setCount = 
+    fun cnt -> 
+        myButton.setAttribute(countAttrName, string cnt)
+        myButton.innerText <- sprintf "You clicked: %i time(s)" cnt
+
+let increaseCount = fun _ -> 
+    myButton.getAttribute countAttrName |> int |> (+) 1 |> setCount
+    
+  //  |> Option.fold (fun unit c -> c + 1 |> string |> myButton.setAttribute "data-count" |> ignore)
 // Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+myButton.onclick <- increaseCount
