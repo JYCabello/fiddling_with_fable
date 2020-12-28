d3c26451b4fc7b76f2ee34a6d1171d59a2ebf2d4
JYCabello <j.cabello.ramos@gmail.com>
2020-12-28 22:47:08 +0100

    Binding for the win

--- FableApp/src/App.fs
+++ FableApp/src/App.fs
@@ -8,8 +8,42 @@ let countAttrName = "data-count"
 
 myButton.setAttribute(countAttrName, string 0)
 
+
+
 let setCount cnt =
         myButton.setAttribute(countAttrName, string cnt)
         myButton.innerText <- sprintf "You clicked: %i time(s)" cnt
     
 myButton.onclick <- fun _ -> myButton.getAttribute countAttrName |> int |> (+) 1 |> setCount
+
+let addOne x = Some(x + 1)
+
+let log str = console.log(str)
+
+let parseInt str =
+    let isSuccessful, number = System.Int32.TryParse str
+    if isSuccessful then Some(number) else None
+
+let (>>=) a f = Option.bind f a
+let (>>) a f = Option.map f a
+
+let a = parseInt "3"
+let b = parseInt "6"
+let c = parseInt "4"
+
+let meh = 
+    a >>= fun av ->
+    b >>= fun bv ->
+    c >>= fun cv ->
+        Some(av + bv + cv)
+
+"998343" 
+    |> parseInt
+    >>= addOne
+    >>= fun value -> value |> addOne >> fun i -> i + 1
+    |> fun opt ->
+        match opt with
+        | Some value -> value |> string
+        | None -> "It was not a valid number"
+    |> log
+    |> ignore
